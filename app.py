1df0c335a1e980504fd13d7b7380482571e01bde
walker-617 <133310637+walker-617@users.noreply.github.com>
2023-05-16 10:11:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ client=pymongo.MongoClient("mongodb+srv://walker:walker617@hod-portal.hpemmk8.mo
 db=client["database"]
 
 today_=""
-time_=0
+time_="0"
 check=0
 
 def reload():
+    global today_
     today_=datetime.now().strftime("%A")[:3].lower()
+    global time_
     time_=datetime.now().hour
 
     if today_=="sat" and check==0:
